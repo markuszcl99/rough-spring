1100aea11e59d1733c068c3219659f327887081f
markuszcl99 <e_markuszhang@163.com>
2022-07-07 22:33:37 +0800

    third commit

---
+++ src/main/java/com/markus/springframework/beans/BeanFactory.java
@@ -0,0 +1,10 @@
+package com.markus.springframework.beans;
+
+/**
+ * @author: markus
+ * @date: 2022/7/7 10:32 下午
+ * @Description: Bean工厂
+ * @Blog: http://markuszhang.com/
+ */
+public class BeanFactory {
+}
