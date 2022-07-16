cc1749915d5d7eb192b4a9b6d4487446f36ba73d
markuszcl99 <e_markuszhang@163.com>
2022-07-16 10:57:29 +0800

    ResourceLoader单测

--- src/test/java/com/markus/springframework/test/ResourceApiTest.java
+++ src/test/java/com/markus/springframework/test/ResourceApiTest.java
@@ -1,5 +1,14 @@
 package com.markus.springframework.test;
 
+import cn.hutool.core.io.IoUtil;
+import com.markus.springframework.core.io.DefaultResourceLoader;
+import com.markus.springframework.core.io.Resource;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * @author: markus
  * @date: 2022/7/16 10:41 上午
@@ -8,5 +17,34 @@ package com.markus.springframework.test;
  * It's my honor to share what I've learned with you!
  */
 public class ResourceApiTest {
+    private DefaultResourceLoader resourceLoader;
 
+    @Before
+    public void init() {
+        resourceLoader = new DefaultResourceLoader();
+    }
+
+    @Test
+    public void test_classpath() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:META-INF/important.properties");
+        InputStream is = resource.getInputStream();
+        String content = IoUtil.readUtf8(is);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_file() throws IOException {
+        Resource resource = resourceLoader.getResource("src/main/resources/META-INF/important.properties");
+        InputStream is = resource.getInputStream();
+        String content = IoUtil.readUtf8(is);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_url() throws IOException {
+        Resource resource = resourceLoader.getResource("https://github.com/markuszcl99/rough-spring/blob/main/src/main/resources/META-INF/important.properties");
+        InputStream is = resource.getInputStream();
+        String content = IoUtil.readUtf8(is);
+        System.out.println(content);
+    }
 }
