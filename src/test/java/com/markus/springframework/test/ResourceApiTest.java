f00ebc8f97598df87f79a6aa0d3dfaa01da7ec5a
markuszcl99 <e_markuszhang@163.com>
2022-07-16 10:45:32 +0800

    配置文件上传 用于测试URLResource

---
+++ src/test/java/com/markus/springframework/test/ResourceApiTest.java
@@ -0,0 +1,12 @@
+package com.markus.springframework.test;
+
+/**
+ * @author: markus
+ * @date: 2022/7/16 10:41 上午
+ * @Description: 测试Resource实现类是否正常
+ * @Blog: http://markuszhang.com/doc-blog/
+ * It's my honor to share what I've learned with you!
+ */
+public class ResourceApiTest {
+
+}
